7f9ce5c452c2542a0f789769d4e4972f011d7185
Katta <manibhargavi.katta@fiserv.com>
2023-11-01 15:16:40 -0400

    finished

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -13,12 +14,7 @@ class Node<T> {
 }
 
 class Solution {
-  public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
-  }
-
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +25,25 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    
+    System.out.println(Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
+  }
+
+   public static List<String> linkedListValues(Node<String> head) {
+    // todo
+    Node<String> pointer = head; 
+    List<String> linkedList = new ArrayList<>(); 
+
+    while(pointer.next!=null){
+      linkedList.add(pointer.val);
+      pointer = pointer.next; 
+
+      if(pointer.next==null){
+        linkedList.add(pointer.val); 
+        break; 
+      }
+    }
+    return linkedList;
   }
 }
